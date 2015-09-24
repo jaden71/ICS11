2a29ed6b39db8b9625e3a769ef80645cbd492b39
Jaden Wang <jadenyjw@gmail.com>
2015-09-24 10:01:33 -0400

    First commit

---
+++ A2_Wang_Jaden.java
@@ -0,0 +1,37 @@
+/*******************************************************************
+  * Assignment: Assignment 2
+  * Description: A Mad-Lib type application.
+  * Name: Jaden Wang
+  * Date: 
+  * Course: ICS3U1
+  * ****************************************************************/
+//import required classes
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class A2_Wang_Jaden {
+  
+  public static void main(String[] args) { 
+    /****************************************************************
+    Variable Dictionary
+    String province = This is a province string inputted by user.
+    String male = This is a male name string inputted by user.
+    String female = This is a female name inputted by user.
+    String city = This is a city string inputted by user.
+    int wholeNum This is the time inputted by user.
+    double realNum = This is the amount of kilometers inputted by user.
+    String comment = This is the user's comment.
+    ****************************************************************/
+    //Declare variables for user input
+    String province;
+    String male;
+    String female;
+    String city;
+    int wholeNum;
+    double realNum;
+    String comment;
+    
+    
+    
+  }//end main
+}//end class
