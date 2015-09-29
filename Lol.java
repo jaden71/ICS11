a3034cc431d6a986db739004e5a53de2f8954e3a
Jaden Wang <jadenyjw@gmail.com>
2015-09-29 09:13:15 -0400

    clean up

---
+++ Lol.java
@@ -0,0 +1,23 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Lol {
+
+  public static void main(String[] args) { 
+  String c = "cat";
+  String d = "dog";
+  String s = c;
+
+   c = d;
+   d = s;
+   s = "bunny";
+
+   System.out.println("c is " + c);
+   System.out.println("d is " + d);
+   System.out.println("s is " + s);
+  
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
